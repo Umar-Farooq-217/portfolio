883ee93454d27b8919bcf9d43cdd73829753f454
Your Name <uf29664@gmail.com>
2024-01-12 23:57:27 +0500

    portfolio

--- app/components/imageSlider/ImageSlider.js
+++ app/components/imageSlider/ImageSlider.js
@@ -1,47 +1,41 @@
-'use client'
-import { useState } from 'react';
-import Image from 'next/image';
+'use client'// components/ImageSlider.js// components/ImageSlider.js
+import React, { useEffect, useState } from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-const ImageSlider= ({ images }) => {
-    const [currentSlide, setCurrentSlide] = useState(0);
-  
-    const nextSlide = () => {
-      setCurrentSlide((prev) => (prev === images.length - 1 ? 0 : prev + 1));
-    };
-  
-    const prevSlide = () => {
-      setCurrentSlide((prev) => (prev === 0 ? images.length - 1 : prev - 1));
-    };
-  
-    return (
-      <div className="relative w-full h-64 md:h-96 lg:h-128">
-        {images.map((image, index) => (
-          <Image
-          width={100}
-          height={100}
-            key={index}
-            src={image}
-            alt={`Slide ${index}`}
-            className={`absolute w-full h-full object-cover transition-opacity ${
-              index === currentSlide ? 'opacity-100' : 'opacity-0'
-            }`}
-            style={{ zIndex: index === currentSlide ? 10 : 1 }}
-          />
-        ))}
-        <button
-          className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-gray-800 text-white px-3 py-1 rounded-md z-20"
-          onClick={prevSlide}
-        >
-          Prev
-        </button>
-        <button
-          className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-gray-800 text-white px-3 py-1 rounded-md z-20"
-          onClick={nextSlide}
-        >
-          Next
-        </button>
-      </div>
-    );
+const ImageSlider = ({ images }) => {
+  const [Slideer, setSlideer] = useState(null);
+
+  useEffect(() => {
+    // Dynamically import react-slick when component mounts on the client side
+    import('react-slick').then((slick) => {
+      setSlideer(slick.default);
+    });
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
   };
 
+  return (
+    Slideer && (
+      <div>
+        <Slideer {...settings}>
+          {images.map((image, index) => (
+            <div key={index}>
+              <img src={image} alt={`Slide ${index + 1}`} />
+            </div>
+          ))}
+        </Slideer>
+      </div>
+    )
+  );
+};
+
 export default ImageSlider;
+
+
