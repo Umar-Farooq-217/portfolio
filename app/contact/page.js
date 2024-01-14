cd804650230c0f2d5459afcbe15fd4f4773f63d7
Your Name <uf29664@gmail.com>
2024-01-14 21:29:18 +0500

    portfolio

--- app/contact/page.js
+++ app/contact/page.js
@@ -4,8 +4,32 @@ import Navbar from '../components/navbar/Navbar'
 export default function page() {
   return (
     <div>
+    <div className='bg-[url("/bg.jpg")]  bg-cover h-screen '>
     <Navbar/>
-    conrac
+
+
+    <div  className='bg-[url("/cardbg.jpg")] font-bold text-white lg:mx-24 md:mx-16 sm:mx-0 mt-24  '>
+    <h1 className='text-5xl pb-6 text-center pt-6'>Fill This Form</h1>
+    <div className='text-center'>
+    <div>
+    <label className='text-2xl py-1 ' htmlFor="name">Enter Your Name:</label>
+    <input className='py-1 my-4 px-3 w-[250px] ml-5 text-black ' type="text" id='name' placeholder='Enter Your Name' />
+    </div>
+    <div>
+    <label className='text-2xl py-1 ' htmlFor="name">Enter Your Name:</label>
+    <input className='py-1 my-4 px-3 ml-5 w-[250px] text-black' type="text" id='name' placeholder='Enter Your Name' />
+    </div>
+    <div className='pb-10'>
+    <label className='text-2xl py-1 ' htmlFor="name">Enter Your Name:</label>
+    <input className='py-1 my-4 px-3 ml-5 text-black w-[250px]' type="text" id='name' placeholder='Enter Your Name' />
+    </div>
+   
+    </div>
+    
+    </div>
+
+    </div>
+    
     
     </div>
   )
